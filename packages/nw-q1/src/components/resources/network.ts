d2aa1d9f3826fadf5edfd998d1588b75cb94f1e9
knakayama <knakayama.sh@gmail.com>
2022-08-05 13:49:44 +0800

    update

--- packages/nw-q1/src/components/resources/network.ts
+++ packages/nw-q1/src/components/resources/network.ts
@@ -1,21 +1,15 @@
 import { Construct } from 'constructs'
-import { TerraformOutput, Resource } from 'cdktf'
-import { ec2 } from '@cdktf/provider-aws'
+import { Resource } from 'cdktf'
+import { vpc } from '@cdktf/provider-aws'
 
 export class Network extends Resource {
   constructor(scope: Construct, name: string) {
     super(scope, name)
 
-    const instance = new ec2.Instance(this, 'compute', {
-      ami: 'ami-01456a894f71116f2',
-      instanceType: 't2.micro',
-      tags: {
-        Name: 'test',
-      },
-    })
-
-    new TerraformOutput(this, 'public-ip', {
-      value: instance.publicIp,
+    new vpc.Vpc(this, 'this', {
+      cidrBlock: '192.168.0.0/16',
+      enableDnsSupport: true,
+      enableDnsHostnames: true,
     })
   }
 }
--- packages/nw-q1/src/components/stacks/nwQ1.ts
+++ packages/nw-q1/src/components/stacks/nwQ1.ts
@@ -3,7 +3,7 @@ import { TerraformStack } from 'cdktf'
 import { AwsProvider } from '@cdktf/provider-aws'
 import { awsRegion } from '../../modules/utils'
 import { Network } from '../resources/network'
-import { Compute } from '../resources/compute'
+//import { Compute } from '../resources/compute'
 
 export class NWQ1Stack extends TerraformStack {
   constructor(scope: Construct, name: string) {
@@ -11,9 +11,14 @@ export class NWQ1Stack extends TerraformStack {
 
     new AwsProvider(this, 'aws', {
       region: awsRegion(),
+      defaultTags: {
+        tags: {
+          Name: 'nw-q1',
+        },
+      },
     })
 
     new Network(this, 'network')
-    new Compute(this, 'compute')
+    //new Compute(this, 'compute')
   }
 }
